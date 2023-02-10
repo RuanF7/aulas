91716c47cd211717341c1c67eb5ac091b54624c0
Ruan Finamor <ruanfinamor@gmail.com>
2023-02-10 14:31:52 -0300

    feat: focus email & password OnkeyUp and Down

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,10 +1,16 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 
 export const Login = () => {
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-   
-    const handleEntrar = useCallback (() => {        
+
+    const emailLength = useMemo(() => {
+        return email.length * 1000;
+    }, [email.length]);
+
+    const handleEntrar = useCallback(() => {
         console.log(email)
         console.log(password)
     }, [email, password]);
@@ -12,20 +18,29 @@ export const Login = () => {
     return (
         <div>
             <form>
+                <p>Quantidade de caracteres no email: {emailLength}</p>
                 <label>
                     <span>Email</span>
-                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                    <input
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        onKeyDown={e => e.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined}
+                    />
                 </label>
 
                 <label>
                     <span>Senha</span>
-                    <input type={"password"} value={password} onChange={e => setPassword(e.target.value)}/>
+                    <input 
+                        type={"password"} 
+                        value={password} 
+                        ref={inputPasswordRef}
+                        onChange={e => setPassword(e.target.value)} />
                 </label>
 
                 <button type="button" onClick={handleEntrar}>
                     Entrar
                 </button>
-            </form>    
+            </form>
         </div>
     );
 }
