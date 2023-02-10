66057a35f0fe7eba6d742eb01d3b45acbcfc8e1a
Ruan Finamor <ruanfinamor@gmail.com>
2023-02-10 10:51:49 -0300

    Criando botão e pagina login

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,12 +1,13 @@
 import { Navigate } from "react-router-dom";
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Dashboard } from "../pages";
+import { Dashboard, Login } from "../pages";
 
 export const Routes1 = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/pagina-inicial" element={<Dashboard/>} />
+                <Route path="/entrar" element={<Login/>} />
 
                 <Route path="*" element={<Navigate to="/pagina-inicial"/>} />
             </Routes>
