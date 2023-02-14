ba6624f51844cf353037ffaa5c078dce2559d8d5
Ruan Finamor <ruanfinamor@gmail.com>
2023-02-14 09:40:34 -0300

    refactor: Dashboard

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,28 +1,37 @@
-import { useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { useCallback, useState } from 'react';
 
-import { useUsuarioLogado } from '../../shared/hooks';
 
 export const Dashboard = () => {
-    const counterRef = useRef(0);
-    
-    const {nomeDoUsuario, logout} = useUsuarioLogado ();
+    const [lista, setLista] = useState<string[]>(['teste', 'teste', 'teste']);
+        
+    const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback ((e) => {
+        if (e.key === 'Enter') {
+            if (e.currentTarget.value.trim().length === 0) return;
 
-    
+            const value = e.currentTarget.value;
+
+            e.currentTarget.value = '' ;
+            
+            setLista((oldLista) => {
+                if (oldLista.includes(value)) return oldLista;
+                return [...oldLista, value];
+            });
+        }
+    }, []);
+   
     return (
         <div>
-            <p>Dashboard</p>
+            <p>Lista</p>
 
-            <p>{nomeDoUsuario}</p>
+            <input
+                onKeyDown={handleInputKeyDown} />
 
-            <p>Contador: {counterRef.current}</p>
+            <ul>
+                {lista.map((value) => {
+                    return <li key={value}>{value}</li>;
+                })}
+            </ul>
 
-            <button onClick={() => counterRef.current++}>Somar</button>
-            <button onClick={() => console.log(counterRef.current)}>Log</button>
-
-            <button onClick={logout}>Logout</button>
-            
-            <Link to="/entrar">Login</Link>
         </div>
     );
 }
