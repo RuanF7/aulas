578cead6d07fbe6f9d8396027b388b2c0332c97b
Ruan Finamor <ruanfinamor@gmail.com>
2023-02-14 18:48:29 -0300

    feat: service

--- src/app/shared/services/api/tarefas/TarefasService.ts
+++ src/app/shared/services/api/tarefas/TarefasService.ts
@@ -1,21 +1,73 @@
+import { Api } from "../ApiConfig";
+import { ApiException } from "../ErrorException";
+interface ITarefa {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+}
 
-const getAll = () => { };
+const getAll = async (): Promise<ITarefa[] | ApiException> => {
 
-const getById = () => { };
+  try {
+    const { data } = await Api().get('./tarefas');
+    return data;
+  } catch ( error: any ) {
+    return new ApiException(error.message || 'Erro ao buscar os registros.');
 
-const create = () => { };
+   }
+  };
+  const getById  = async (id: number): Promise<ITarefa | ApiException> => {
 
-const updateById = () => { };
+    try {
+      const { data } = await Api().get(`./tarefas/${id}`);
+      return data;
+    } catch ( error: any ) {
+      return new ApiException(error.message || 'Erro ao consultar o registro.');
+  
+     }
+    };
+  
 
-const deleteById = () => { };
+  const create  = async (dataToCreate: Omit<ITarefa, 'id'>): Promise<ITarefa[] | ApiException> => {
+
+    try {
+      const { data } = await Api().post<any>('./tarefas', dataToCreate);
+      return data;
+    } catch ( error: any ) {
+      return new ApiException(error.message || 'Erro ao criar registro.');
+  
+     }
+    };
+
+  const updateById  = async (id: string, dataToUpdate: ITarefa): Promise<ITarefa[] | ApiException> => {
+
+    try {
+      const { data } = await Api().put(`./tarefas/${id}`, dataToUpdate);
+      return data;
+    } catch ( error: any ) {
+      return new ApiException(error.message || 'Erro ao atualizar o registro.');
+  
+     }
+    };
+
+  const deleteById  = async (id: string): Promise<undefined | ApiException> => {
+
+    try {
+      const { data } = await Api().get(`./tarefas/${id}`);
+      return undefined;
+    } catch ( error: any ) {
+      return new ApiException(error.message || 'Erro ao apagar o registro.');
+  
+     }
+    };
 
 
 
 
-export const tarefasService = {
-  getAll,
-  getById,
-  create,
-  updateById,
-  deleteById
-};
+  export const TarefasService = {
+    getAll,
+    getById,
+    create,
+    updateById,
+    deleteById
+  };
