f66c74f33fece9eccc63e8b2efa4721d83ea24b3
Ruan Finamor <ruanfinamor@gmail.com>
2023-02-16 10:45:29 -0300

    refactor: Remove arquivos desnecessários

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,6 @@
 import { useCallback, useEffect, useState } from 'react';
 import { ApiException } from '../../shared/services/api/ErrorException';
 import { ITarefa, TarefasService } from '../../shared/services/api/tarefas/TarefasService';
-import './Dashboard.css';
 
 export const Dashboard = () => {
     const [lista, setLista] = useState<ITarefa[]>([]);
